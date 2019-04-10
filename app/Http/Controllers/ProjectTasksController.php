b969bdca75bb5c1333a5e8ff614c79e5cb3203b5
Joseph Herbert Tandingan <jh.tandingan@coi.local>
2019-04-10 18:51:42 +0800

    A bit of complex code "episode 20"

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -17,13 +17,4 @@ class ProjectTasksController extends Controller
 
         return back();
     }
-
-    public function update(Task $task)
-    {        
-        $method = request()->has('completed') ? 'complete' : 'incomplete';
-
-        $task->$method();
-
-        return back();
-    }
 }
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -14,8 +14,11 @@
         <div class="box">
             @foreach ($project->tasks as $task)
                 <div>
-                    <form method="POST" action="/tasks/{{ $task->id }}">
-                        @method('PATCH')
+                    <form method="POST" action="/completed-tasks/{{ $task->id }}">
+                        @if ($task->completed)
+                            @method('DELETE')
+                        @endif
+                        
                         @csrf
 
                         <label for="completed" class="checkbox {{ $task->completed ? 'is-complete' : '' }}">
